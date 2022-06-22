5c444dec6fb809f7b31337632b6e9960140c1e34
safa954 <100359594+safa954@users.noreply.github.com>
2022-06-22 07:05:42 -0700

    main

--- 21-JUNE-2022Tasks/Task1/src/Components/Wellcom.js
+++
@@ -1,8 +0,0 @@
-function Wellcom() {
-    const st = { color: "red" }
-    return ( <div className="container">< h3 > Title: Wellcom </h3>
-    <p style={st}> Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry 's standard dummy.</p>
-    </div>
-     );
-    }
-    export default Wellcom;
--- 21-JUNE-2022Tasks/Task2/src/Components/Nav.js
+++
@@ -1,6 +0,0 @@
-function Nav(){
-    return ( <nav class = "navbar navbar-dark bg-dark text-white" >
-     <h1> This is our site</h1>
-    </nav>);
-    }
-    export default Nav ;
