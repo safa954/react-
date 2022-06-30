539c0577e30fde4a4570bcbbdf6577551a11ab63
safa954 <100359594+safa954@users.noreply.github.com>
2022-06-30 02:15:50 -0700

    main

---
+++ 29-6/hooks/src/App.js
@@ -0,0 +1,13 @@
+import react from "react";
+
+import Example from "./hooks";
+
+function App() {
+  return (
+   <div>
+<Example/>
+   </div>
+  );
+}
+
+export default App;
---
+++ task30-6/my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import UseEffect from './component/UseEffect';
+
+function App() {
+return (
+<div className="App">
+<UseEffect />
+</div>
+);
+}
+
+export default App;
