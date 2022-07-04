45c9fde3f1d229b8d0751ab1a6a2b00da1fe6f6b
safa954 <100359594+safa954@users.noreply.github.com>
2022-07-04 07:23:31 -0700

    main

--- context3-7/task/src/App.js
+++ context3-7/task/src/App.js
@@ -1,36 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import MovieList from './components/MovieList';
-import AboutUs from './components/AboutUs';
-import SingleMovie from './components/SingleMovie';
-import React, { useEffect, useState } from "react";
-
-
+import Login  from './components/Login';
 function App() {
-
-  const [movies, setMovies] = useState([]);
-
-    useEffect(() => {
-        fetch('https://api.themoviedb.org/3/movie/popular?api_key=6bd2f87c212a016945bb7783c8671cc2&language=en-US&page=1')
-            .then((response) => response.json())
-            .then(moviesList => {
-                setMovies(moviesList.results);
-            });
-    }, []);
-
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movielist" element={<MovieList movies={movies} />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/movie/:id" element={<SingleMovie />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+   <Login />
+    </div>
   );
 }
 
